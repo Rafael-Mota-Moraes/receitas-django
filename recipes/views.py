68b86ccd551876301b95b75a45fbe12e4b23b2ab
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-12-14 17:00:23 -0300

    adicionando font awesome no template

--- recipes/views.py
+++ recipes/views.py
@@ -7,14 +7,3 @@ def home(request):
     return render(request, 'recipes/home.html', status=404, context={
         'name': 'Rafael Moreira'
     })
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-# Create your views here.
