883d765d9f008c8b54ea09ee2b9d258f2a8be196
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-12-14 11:29:13 -0300

    Movendo o código para o app recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('HOME')
+
+
+def sobre(request):
+    return HttpResponse('SOBRE')
+
+
+def contato(request):
+    return HttpResponse('CONTATO')
+
 
 # Create your views here.
