a81eda08c97ebdb43de362d2ce40431eb47d9d26
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-06-29 12:30:03 -0300

    Criando a header, separando um "partial" para head e usando include no template

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Rafael'})
+    return render(request, 'recipes/pages/home.html', context={'name': 'Rafael'})
