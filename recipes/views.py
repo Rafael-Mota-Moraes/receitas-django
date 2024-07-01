d4c8ee8b12b4ae460a2c187d1a17ae9c860f3eaf
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-07-01 11:31:33 -0300

    diferenças entre views de lista e detalhe

--- recipes/views.py
+++ recipes/views.py
@@ -5,8 +5,14 @@ from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'recipes': [make_recipe() for _ in range(10)]})
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+        'is_detail_page': False
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe()})
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True
+    })
