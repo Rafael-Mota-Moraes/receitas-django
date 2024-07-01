910db59593ba0360cd14eac603b7da5612e32422
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-07-01 09:33:32 -0300

    view de recipe

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={'name': 'Rafael'})
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={'name': 'Rafael'})
