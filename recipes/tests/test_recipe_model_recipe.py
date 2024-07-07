73f5c9c017e24b2e469ed2c498c27f2e8a706e0b
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-07-07 12:24:48 -0300

    Criando testes parametrizados para max_length dos campos

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,5 +1,6 @@
 from .test_recipe_base import RecipeTestBase
 from django.core.exceptions import ValidationError
+from parameterized import parameterized, parameterized_class
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -9,5 +10,16 @@ class RecipeModelTest(RecipeTestBase):
 
     def test_recipe_title_has_raises_error_if__title_has_more_than_65_chars(self):
         self.recipe.title = 'A' * 70
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()  # validação ocorre aqui!
+
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_lenght(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()  # validação ocorre aqui!
