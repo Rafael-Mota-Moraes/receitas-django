34150ee2dd8f2bbf31065fe94f2026216d8ff92a
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-07-23 10:37:55 -0300

    is_valid e save para salvar dados do formulário na base de dados

--- authors/views.py
+++ authors/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
 from .forms import RegisterForm
+from django.contrib import messages
 
 
 def register_view(request):
@@ -16,5 +17,11 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(
+            request, 'Seu usuário foi salvo na base de dados, por favor faça login.')
+        del (request.session['register_form_data'])
 
     return redirect('authors:register')
