6434743b210ec940e9e70c588f5ce7b48852547b
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-07-24 11:41:55 -0300

    testando campos vazios

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase as DjangoTestCase
+from unittest import TestCase
 from authors.forms import RegisterForm
 from parameterized import parameterized
+from django.urls import reverse
 
 
 class AuthorRegisterFormUnitTest(TestCase):
@@ -39,3 +41,25 @@ class AuthorRegisterFormUnitTest(TestCase):
         form = RegisterForm()
         current = form[field].field.label
         self.assertEqual(current, needed)
+
+
+class AuthorRegisterFormIntegrationTest(DjangoTestCase):
+    def setUp(self, *args, **kwargs) -> None:
+        self.form_data = {
+            'username': 'user',
+            'first_name': 'fisrt',
+            'last_name': 'last',
+            'email': 'email@email.com',
+            'password': 'Str0ngP@ssw0rd1',
+            'password2': 'Str0ngP@ssw0rd1',
+        }
+        return super().setUp(*args, **kwargs)
+
+    @parameterized.expand([
+        ('username', 'Campo é obrigatório')
+    ])
+    def test_fields_cannot_be_empty(self, field, message):
+        self.form_data[field] = ''
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(message, response.content.decode('utf-8'))
