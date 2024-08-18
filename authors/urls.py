5a308562fc58dbe54da090d5fceb325eb2ac7f0d
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-08-18 11:38:23 -0300

    Criando uma view usando a classe base django.views.View

--- authors/urls.py
+++ authors/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
         name='dashboard_recipe_new'
     ),
     path("dashboard/recipe/<int:id>/edit/",
-         views.dashboard_recipe_edit, name="dashboard_recipe_edit"),
+         views.DashboardRecipe.as_view(), name="dashboard_recipe_edit"),
     path("dashboard/recipe/<int:id>/delete/",
          views.dashboard_recipe_delete, name="dashboard_recipe_delete"),
 ]
---
+++ authors/views/dashboard_recipe.py
@@ -0,0 +1,43 @@
+from django.http import Http404
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.views import View
+from django.contrib import messages
+
+from authors.forms.recipe_form import AuthorRecipeForm
+from recipes.models import Recipe
+
+
+class DashboardRecipe(View):
+    def get(self, request, id):
+        self.request
+        recipe = Recipe.objects.get(
+            is_published=False,
+            author=request.user,
+            pk=id,
+        )
+
+        if not recipe:
+            raise Http404()
+
+        form = AuthorRecipeForm(
+            data=request.POST or None,
+            files=request.FILES or None,
+            instance=recipe
+        )
+
+        if form.is_valid():
+            # Agora o form é valido e eu posso tentar salvar
+            recipe = form.save(commit=False)
+            recipe.author = request.user
+            recipe.preparation_steps_is_html = False
+            recipe.is_published = False
+
+            recipe.save()
+            messages.success(request, "Sua receita foi salva com sucesso!")
+
+            return redirect(reverse("authors:dashboard_recipe_edit", args=(id, )))
+
+        return render(request, 'authors/pages/dashboard_recipe.html', {
+            "form": form,
+        })
