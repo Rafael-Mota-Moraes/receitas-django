bbcf3ec1cd201e4df0cc06662349c9a45d9dd596
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-08-22 17:53:04 -0300

    Corrigindo os testes da DetailView

--- recipes/tests/test_recipe_detail_view.py
+++ recipes/tests/test_recipe_detail_view.py
@@ -6,17 +6,18 @@ from unittest import skip
 
 class RecipeDetailViewTest(RecipeTestBase):
     def test_recipe_detail_view_function_is_correct(self):
-        view = resolve(reverse("recipes:recipe", kwargs={"id": 1}))
-        self.assertIs(view.func, views.recipe)
+        view = resolve(reverse("recipes:recipe", kwargs={"pk": 1}))
+        self.assertIs(view.func.view_class, views.RecipeDetail)
 
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
-        response = self.client.get(reverse("recipes:recipe", kwargs={"id": 1000}))
+        response = self.client.get(
+            reverse("recipes:recipe", kwargs={"pk": 1000}))
         self.assertEqual(response.status_code, 404)
 
     def test_recipe_detail_template_loads_the_correct_recipe(self):
         needed_title = "Detail page test"
         self.make_recipe(title=needed_title)
-        response = self.client.get(reverse("recipes:recipe", kwargs={"id": 1}))
+        response = self.client.get(reverse("recipes:recipe", kwargs={"pk": 1}))
 
         content = response.content.decode("utf-8")
 
@@ -25,6 +26,7 @@ class RecipeDetailViewTest(RecipeTestBase):
     def test_recipe_detail_template_dont_load_recipes_not_published(self):
         """Test recipe is published False dont show"""
         recipe = self.make_recipe(is_published=False)
-        response = self.client.get(reverse("recipes:category", args=(recipe.id,)))
+        response = self.client.get(
+            reverse("recipes:category", args=(recipe.id,)))
 
         self.assertEqual(response.status_code, 404)
